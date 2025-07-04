001eff90347282f17e9faefd8ae4a65e9b3ea64d
JOSE-appu <jose.seban2012@gmail.com>
2025-07-04 09:48:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,26 +1,7 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
-
 terraform {
-  required_providers {
-    docker = {
-      source = "kreuzwerker/docker"
-    }
-  }
+  required_version = ">= 1.0.0"
 }
 
-provider "docker" {}
-	
-resource "docker_image" "nginx" {
-  name         = "nginx:latest"
-  keep_locally = false
-}
-
-resource "docker_container" "nginx" {
-  image = docker_image.nginx.name
-  name  = "nginx"
-  ports {
-    internal = 80
-    external = 8000
-  }
+output "greeting" {
+  value = "Hello from Terraform!"
 }
